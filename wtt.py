3ffa2cf2efc475744d275f1a458e3b28058d8be6
peder2911 <pglandsverk@gmail.com>
2020-01-23 16:18:29 +0100

    Initial
    Functional.
    Things to add:
    
    - Tests
    - Perhaps remove some dependencies? (Just laziness...)

---
+++ wtt.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import watchgod
+import fire
+import yaml
+import os
+import subprocess
+
+
+def watchthis(dir=".",config="watch.yml"):
+
+    print("Watching {dir}".format(dir=os.path.abspath(dir)))
+
+    with open(config) as f:
+        config = yaml.safe_load(f)
+
+    formats = [e["filetype"] for e in config]
+    print("Watching these formats:\n{formats}".format(formats = "\n".join(formats)))
+
+    for c in watchgod.watch(dir):
+        change, filepath = c.pop()
+        _, ext = os.path.splitext(filepath)
+
+        for e in config:
+            if ext == e["filetype"]:
+                call = e["call"].format(file=filepath).split()
+                subprocess.call("clear")
+                subprocess.call(call)
+
+
+if __name__ == "__main__":
+    fire.Fire(watchthis)
+
