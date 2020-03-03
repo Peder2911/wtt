ca3ba8f962411f35e3a5c8d6f8e2d4f9658d81cd
peder2911 <pglandsverk@gmail.com>
2020-03-03 13:44:22 +0100

    updates

--- wtt.py
+++ wtt.py
@@ -7,12 +7,13 @@ import os
 import subprocess
 
 
-def watchthis(dir=".",config="watch.yml"):
+def watchthis(dir=".",config=os.path.expanduser("~/.wtt.yaml")):
 
     print("Watching {dir}".format(dir=os.path.abspath(dir)))
 
     with open(config) as f:
         config = yaml.safe_load(f)
+        
 
     formats = [e["filetype"] for e in config]
     print("Watching these formats:\n{formats}".format(formats = "\n".join(formats)))
